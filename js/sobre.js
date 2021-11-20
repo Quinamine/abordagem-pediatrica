c3de87b5543009855d2b9b33b003b08a97392f57
Quinamine <Dery.felix.df@gmail.com>
2021-11-20 02:26:25 -0300

    restruturei todo layout

--- js/sobre.js
+++ js/sobre.js
@@ -1,16 +1,1 @@
-let content = "Conheça a História da Vida de Adérito Félix Quinamine (o criador da plataforma \"Abordagem Pediátrica\")";
-let control = 0;
-
-function escreva() {
-
-	let banner = document.querySelector(".banner");
-	let banner_hdois = document.querySelector(".banner-h2");
-	if(control<content.length){
-		banner_hdois.innerHTML += content[control];
-		control++;
-
-		setTimeout(escreva, 100)
-	}
-}
-
-window.addEventListener("load", escreva);
+let content='Conheça a História da Vida de Adérito Félix Quinamine (o criador da plataforma "Abordagem Pediátrica")',control=0;function escreva(){document.querySelector(".banner");let e=document.querySelector(".banner-h2");control<content.length&&(e.innerHTML+=content[control],control++,setTimeout(escreva,100))}window.addEventListener("load",escreva);
