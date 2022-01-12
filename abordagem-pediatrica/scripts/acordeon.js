ca9013d4514c8429d7b037ae522440ceba9acee9
Quinamine <Dery.felix.df@gmail.com>
2022-01-11 22:40:42 -0300

    termine a pagina de contacto

--- abordagem-pediatrica/scripts/acordeon.js
+++ abordagem-pediatrica/scripts/acordeon.js
@@ -1,6 +1,6 @@
 
 
-let  orderIndex = 0; /* Variável de controle da função prevOrnextImg */
+
 const Acordeon = {
 
     changeHeaderBg: () => {
@@ -23,74 +23,36 @@ const Acordeon = {
         g.contains("aberta") ? g.remove("aberta") : g.add("aberta");
     },
     
-    changeBgButtons: (btDir, btIndex, action) => {
-       if(action=="applyBg") {
-           btDir[btIndex].classList.add("hover");
-       }
-       else {
-        btDir[btIndex].classList.remove("hover");
-       }
-    },
-
-    prevOrnextImg: (dir, albumIndex) => {
-        let imgs = album[albumIndex].querySelectorAll("img");
-    
-        for (const img of imgs) {
-            img.classList.remove("ordem-zero");
-        }
-    
-        if(dir==1){
-            imgs[orderIndex+dir].classList.add("ordem-zero");
-            orderIndex++;
-        }
-    
-        else {
-            imgs[orderIndex-1].classList.add("ordem-zero");
-            --orderIndex;
-        }
-    
-    
-        if(orderIndex==0) {
-            prevButtons[albumIndex].classList.add("hide");
-        }
-       
-        else {
-            prevButtons[albumIndex].classList.remove("hide");
-        }
-
-        if(orderIndex>=(imgs.length-1)) {
-            nextButtons[albumIndex].classList.add("hide");
-        }
-       
-        else {
-            nextButtons[albumIndex].classList.remove("hide");
-        }  
-    },
-      
     countImgs: (indice_da_galeria) => {
         let imgs = galeria[indice_da_galeria].querySelectorAll(".contentor-das-imagens img");
         let containerOfImgLen = galeria[indice_da_galeria].querySelector(".numero-de-itens");
 
         containerOfImgLen.innerHTML = imgs.length + (imgs.length>1 ? " itens" : " item");
+    },
+
+    changeImg: (containerIndex, currentImgIndex) => {
+        let imgs = album[containerIndex].querySelectorAll("img");
+        let imgButtons = album[containerIndex].querySelectorAll("button");
+    
+        for (let i = 0; i<imgs.length; i++) {
+            imgs[i].classList.remove("ordem-zero");
+            imgButtons[i].classList.remove("clicado")
+        }
+    
+        imgs[currentImgIndex].classList.add("ordem-zero");
+        imgButtons[currentImgIndex].classList.add("clicado");
     }
 }
 
 let galeria,
     titulos_da_galeria,
-    album,
-    nextButtons,
-    prevButtons; 
+    album;
 
 function init() {
     /* Inicialização das variáveis */
     galeria = document.querySelectorAll(".galeria"); /* CountImgs */
 
     titulos_da_galeria = document.querySelectorAll(".titulo-da-galeria"); // ChangeHeaderBg 
-
-    album = document.querySelectorAll(".contentor-das-imagens"); // prevOrnextImg
-
-    nextButtons = document.querySelectorAll(".avancar"); // prevOrnextImg
-    prevButtons = document.querySelectorAll(".voltar"); // prevOrnextImg
 
     /** MUDANÇA DO FUNDO DO CABEÇALHO DO ACORDEON ON MOUSEOVER */
     Acordeon.changeHeaderBg();
@@ -101,39 +63,7 @@ function init() {
             Acordeon.showImages(titulo.dataset.indice_da_galeria);
         })
     });
-
-
-
-    /** AVANÇAR OU VOLTAR IMAGENS */
-    nextButtons.forEach( nextBt => {
-        nextBt.addEventListener("click", () => {
-            Acordeon.prevOrnextImg(1, nextBt.dataset.index);
-        })
-
-        nextBt.addEventListener("mouseover", () => {
-            Acordeon.changeBgButtons(nextButtons, nextBt.dataset.index, "applyBg");
-        })
-
-        nextBt.addEventListener("mouseleave", () => {
-            Acordeon.changeBgButtons(nextButtons, nextBt.dataset.index, "removeBg");
-        })
-
-    })
-
-    prevButtons.forEach ( prevBt => {
-        prevBt.addEventListener("click", () => {
-            Acordeon.prevOrnextImg(0, prevBt.dataset.index)
-        })
-
-        prevBt.addEventListener("mouseover", () => {
-            Acordeon.changeBgButtons(prevButtons, prevBt.dataset.index, "applyBg");
-        })
-
-        prevBt.addEventListener("mouseleave", () => {
-            Acordeon.changeBgButtons(prevButtons, prevBt.dataset.index, "removeBg");
-        })
-    })
-
+  
 
     /** COUNTIMAGES */
     titulos_da_galeria.forEach( (e) => {
@@ -141,12 +71,22 @@ function init() {
     })
 
 
+    /** CHANGE IMG */
+    album = document.querySelectorAll(".contentor-das-imagens");
+
+    let imgButtons = document.querySelectorAll(".contentor-das-imagens button")
+
+    imgButtons.forEach( bt => {
+        bt.addEventListener("click", () => {
+            Acordeon.changeImg(bt.dataset.parentpos, bt.dataset.selfpos);
+        })
+    })
+
 
     /** Manter Acordeon Fechado */
     if(window.innerWidth>=768) {
         document.querySelector(".galeria.tmg").classList.add("aberta");
-    }
-    
+    } 
 }
 
 
--- abordagem-pediatrica/scripts/slide.js
+++ abordagem-pediatrica/scripts/slide.js
@@ -3,10 +3,9 @@
 let indiceActual = 1,
     tempoSlide = 4500;
 
-
-
+let pictures;
 function slide() {
-    let pictures = document.querySelectorAll(".banner-rotativo picture");
+    pictures = document.querySelectorAll(".banner-rotativo picture");
 
     for (const pic of pictures) {
         pic.classList.remove("pic-actual");
@@ -43,3 +42,46 @@ setInterval(slide, tempoSlide);
 
 try {setInterval(progressaoSlide, tempoSlide)} 
 catch (error) {console.log("")};
+
+
+
+
+function nextImg(currentImgIndex) {
+    let pictures = document.querySelectorAll(".banner-rotativo picture");
+    
+
+    for (let i= 0; i<pictures.length; i++) {
+        pictures[i].classList.remove("pic-actual");
+        bolinhas[i].classList.remove("cheia");
+    }
+
+    pictures[currentImgIndex].classList.add("pic-actual"); 
+    bolinhas[currentImgIndex].classList.add("cheia");
+}
+
+
+
+
+let bolinhas;
+function init() {
+    bolinhas = document.querySelectorAll(".contentor-das-bolinhas-de-progresso div");
+
+    bolinhas.forEach( bola => {
+        bola.addEventListener("click", () => {
+            nextImg(bola.dataset.picindex);
+
+            if((bola.dataset.picindex>=(bolinhas.length-1)) || (bola.dataset.picindex>=(bolinhas.length-1))){
+                indiceActual = 0;
+                bolinha = 0;
+            }
+            else {
+                indiceActual = Number(bola.dataset.picindex)+1;
+                bolinha = Number(bola.dataset.picindex)+1;
+            }
+
+        })
+    })
+}
+
+window.addEventListener("load", init)
+
